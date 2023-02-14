02e7008ef4208510a2eb4b8bb4423298687bd977
Alejandro Hernandez <a.hernandez@traxion.global>
2023-02-13 23:34:29 -0600

    feat: config sns client with arn region

--- aws_lambda_stream/connectors/sns.py
+++ aws_lambda_stream/connectors/sns.py
@@ -1,17 +1,20 @@
 import os
 import boto3
-
+from aws_lambda_stream.utils.aws import get_region
 
 class Connector():
     def __init__(self,topic_arn = os.getenv('TOPIC_ARN')) -> None:
         self.topic_arn = topic_arn
-        self.client = boto3.client('sns')
+        self.client = boto3.client('sns',
+                                   region_name=get_region(topic_arn))
+
     def publish(self, input_params):
         params = {
             'TopicArn': self.topic_arn,
             **input_params
         }
         return self.client.publish(**params)
+
     def publish_batch(self, input_params):
         params = {
             'TopicArn': self.topic_arn,
